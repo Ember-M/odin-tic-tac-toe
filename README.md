07c9881c40ca1f595cce4174322a0c6fcaba5583
Ember-M <ember.maes@gmail.com>
2022-09-26 20:07:56 -0700

    modified:   README.md 	modified:   app.js 	modified:   index.html 	modified:   style.css     Finialized style.     Added restart functionality.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-tic-tac-toe
 TTC Page for The Odin Project
+
+An implementation of (most of) the requirements for TOP's Tic-Tac-Toe. I implemented this following WebDevSimplified's fantastic tutorial (https://www.youtube.com/watch?v=Y-GkMjUZsmM), and then customized it to my own requirements. I added a game start button, player indicators, color styling, and a few extra features. I still want to revisit this at some point and refactor it more in line with the OOP style layed out in the project template.
--- app.js
+++ app.js
@@ -2,11 +2,37 @@ const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
-const gameButton = document.getElementById('startGame');
+const startButton = document.getElementById('startGame');
 const playerTurn = document.getElementById('playerTurn');
+const winningMessage = document.querySelector('[data-winning-message-text]')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton');
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
 let circleTurn;
 
-gameButton.addEventListener('click', startGame, {once: true});
+// Re-Initialize Game State //
+restartButton.addEventListener('click', ()=>{
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener('click', handleClick)
+        winningMessageElement.classList.remove('show');
+})
+    playerTurn.innerText = "";
+    removeBoardHoverClass();
+});
+
+
+startButton.addEventListener('click', startGame)
 
 function startGame(){
     circleTurn = false;
@@ -15,15 +41,36 @@ function startGame(){
     });
     setTurnDisplay(circleTurn);
     setBoardHoverClass()
+    
 }
 
+//Game Logic For Each Game Board Cell //
 function handleClick(e){
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark (cell,currentClass)
-    swapTurns()
-    setBoardHoverClass()
+    if (checkWin(currentClass)){
+        endGame(false);
+    }
+    else if (isDraw()){
+        endGame(true);
+    }
+    else{ 
+        swapTurns()
+        setBoardHoverClass()
+    }
     
+    
+}
+
+function endGame(draw){
+    if (draw){
+        winningMessage.innerText = `It's a draw!`
+    }
+    else{
+        winningMessage.innerText = `${circleTurn ? "Player 2":"Player 1"} Wins!`
+    }
+    winningMessageElement.classList.add('show');
 }
 
 function placeMark(cell, currentClass){
@@ -43,6 +90,8 @@ function setTurnDisplay(turn){
         playerTurn.innerText = "Player Two's Turn!"
     }
 }
+
+// Initialize Mark Hover Display //
 function setBoardHoverClass(){
     board.classList.remove(X_CLASS)
     board.classList.remove(CIRCLE_CLASS);
@@ -52,4 +101,21 @@ function setBoardHoverClass(){
     else{
         board.classList.add(X_CLASS)
     }
+}
+function removeBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS);
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination =>{
+        return combination.every(index =>{
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
+function isDraw(){
+    return [...cellElements].every(cell =>{
+        return cell.classList.contains(X_CLASS)||cell.classList.contains(CIRCLE_CLASS);
+    })
 }
